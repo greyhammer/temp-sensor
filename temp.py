4f3e0cb4657a60ce95892c62f2bbdd425750e718
greyhammer <kjjanak@gmail.com>
2023-09-07 12:28:18 -0500

    adding api

--- temp.py
+++ temp.py
@@ -1,12 +1,21 @@
 import Adafruit_DHT
+from flask import Flask, jsonify, request
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
-while True:
+app = Flask(__name__)
+
+@app.route("/")
+def get_data():
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
     if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+        env_data = {}
+        env_data['temp'] = temperature
+        env_data['hum'] = humidity
+        print(env_data)
+
+        return env_data
     else:
-        print("Failed to retrieve data from humidity sensor")
+        return "Failed to retrieve data from humidity sensor"
